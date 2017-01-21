ab6dd2ddd8486996b87de341925bb745bef29c4c
travertischio <travertischio@gmail.com>
2017-01-21 16:30:36 -0500

    Counts down from input number and prints concat of strings

--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -2,13 +2,12 @@
 
 (defun fizzbuzz (n)
   (cond
+    ((= n 1) (write-to-string 1))
     ((= (mod n 3) 0)
       (cond
-        ((= (mod n 5) 0) (print "FizzBuzz"))
-        ((print "Fizz"))))
-    ((= (mod n 5) 0) (print "Buzz"))
-    ((print n)))
-  (cond
-    ((< n 100) (fizzbuzz (+ n 1)))))
+        ((= (mod n 5) 0) (concatenate 'string "FizzBuzz" (fizzbuzz (- n 1))))
+        ((concatenate 'string "Fizz" (fizzbuzz (- n 1))))))
+    ((= (mod n 5) 0) (concatenate 'string "Buzz" (fizzbuzz (- n 1))))
+    ((concatenate 'string (write-to-string n) (fizzbuzz (- n 1))))))
 
-(fizzbuzz 1)
+(print (fizzbuzz 100))
