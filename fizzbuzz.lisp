3023fdf3a97d9c4c7ccaeaf193b7f9b68efd34f5
travertischio <travertischio@gmail.com>
2017-01-21 17:18:19 -0500

    Prints list of proper fizz buzz

--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -2,12 +2,12 @@
 
 (defun fizzbuzz (n)
   (cond
-    ((= n 1) (write-to-string 1))
+    ((= n 1) (cons n nil))
     ((= (mod n 3) 0)
       (cond
-        ((= (mod n 5) 0) (concatenate 'string "FizzBuzz" (fizzbuzz (- n 1))))
-        ((concatenate 'string "Fizz" (fizzbuzz (- n 1))))))
-    ((= (mod n 5) 0) (concatenate 'string "Buzz" (fizzbuzz (- n 1))))
-    ((concatenate 'string (write-to-string n) (fizzbuzz (- n 1))))))
+        ((= (mod n 5) 0) (cons  "FizzBuzz" (fizzbuzz (- n 1))))
+        ((cons "Fizz" (fizzbuzz (- n 1))))))
+    ((= (mod n 5) 0) (cons "Buzz" (fizzbuzz (- n 1))))
+    ((cons n (fizzbuzz (- n 1))))))
 
-(print (fizzbuzz 100))
+(print (reverse (fizzbuzz 100)))
