ddd548ac1763d5237e74c330c301d53e1afe5f97
travertischio <travertischio@gmail.com>
2017-02-26 11:49:04 -0500

    Reads multiple lines of input in go 3n+1

--- algorithms/go/3nplus1.go
+++ algorithms/go/3nplus1.go
@@ -4,32 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-  "strings"
-  "strconv"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	numbers, _ := reader.ReadString('\n')
-  input := strings.Split(numbers, " ")
-  strI, strJ := input[0], strings.TrimSpace(input[1])
-  i, _ := strconv.ParseInt(strI, 10, 32)
-  j, _ := strconv.ParseInt(strJ, 10, 32)
+	// Scan input
+	scanner := bufio.NewScanner(os.Stdin)
+	var lines []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
 
-  maxCount := 0
-  for k := i; k <= j; k++ {
-    count := 1
-    for l := k; l > 1; count++ {
-      if l%2 == 0 {
-        l = l/2
-      } else {
-        l = l*3 + 1
-      }
-    }
+	// Iterate through array of input
+	for _, value := range lines {
+		input := strings.Split(value, " ")
+		strI, strJ := input[0], strings.TrimSpace(input[1])
+		i, _ := strconv.ParseInt(strI, 10, 32)
+		j, _ := strconv.ParseInt(strJ, 10, 32)
 
-    if count > maxCount {
-      maxCount = count
-    }
-  }
-	fmt.Println(i, j, maxCount)
+		maxCount := 0
+		for k := i; k <= j; k++ {
+			count := 1
+			for l := k; l > 1; count++ {
+				if l%2 == 0 {
+					l = l / 2
+				} else {
+					l = l*3 + 1
+				}
+			}
+
+			if count > maxCount {
+				maxCount = count
+			}
+		}
+		fmt.Println(i, j, maxCount)
+	}
 }
