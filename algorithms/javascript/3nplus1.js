57e8aeaa1569d88c36dc872734f31971447725ce
travertischio <travertischio@gmail.com>
2017-02-26 11:27:48 -0500

    Takes multiple lines of input for javascript 3n+1

--- algorithms/javascript/3nplus1.js
+++ algorithms/javascript/3nplus1.js
@@ -10,21 +10,25 @@ process.stdin.on('end', () => {
 })
 
 const processData = (input) => {
-  const inArr = input.split(' ').map(Number)
-  const i = inArr[0]
-  const j = inArr[1]
+  const inArr = input.split('\n')
+  inArr.pop()
+  inArr.forEach((line) => {
+    const lineArr = line.split(' ').map(Number)
+    const i = lineArr[0]
+    const j = lineArr[1]
 
-  let maxCount = 0
-  for (let k = i; k <= j; k++) {
-    let count = 1
-    for (let l = k; l > 1; count++) {
-      if (l % 2 === 0) {
-        l = l / 2
-      } else {
-        l = 3 * l + 1
+    let maxCount = 0
+    for (let k = i; k <= j; k++) {
+      let count = 1
+      for (let l = k; l > 1; count++) {
+        if (l % 2 === 0) {
+          l = l / 2
+        } else {
+          l = 3 * l + 1
+        }
       }
+      if (count > maxCount) maxCount = count
     }
-    if (count > maxCount) maxCount = count
-  }
-  console.log(inArr.join(' ') + ' ' + maxCount)
+    console.log(lineArr.join(' ') + ' ' + maxCount)
+  })
 }
